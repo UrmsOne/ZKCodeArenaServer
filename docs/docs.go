5089d967f3ca04b6a9250136765283e461a31f74
xiaoTnb <2039242649@qq.com>
2026-01-22 10:31:52 +0800

    修改端口号解决cors问题

--- docs/docs.go
+++ docs/docs.go
@@ -6991,7 +6991,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "8.138.184.24:8080",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "ZK Code Arena API",
